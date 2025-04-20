9363cbea632fd66fc636edeb022b4176870289c9
dpkingii <dupopking@gmail.com>
2025-04-20 12:57:04 -0400

    added a simple login component

--- frontend/appaca/src/pages/LoginPage.tsx
+++ frontend/appaca/src/pages/LoginPage.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { useState } from "react";
-
+import { useNavigate } from 'react-router-dom';
+import "./LoginPage.css"
 
 function LoginPage() {
+    
+    const [userId,setUserId] = useState("");
+    const navigate = useNavigate();
+
+
+    const handleLogin = async () => {
+        navigate(`/display/`)
+    };
+
     return (
-        <h1>Login Page Placeholder</h1>
+        <div className="login-container">
+            <h1>🦙 Appaca 🦙</h1>
+            <input
+            onChange={(event) => {
+                setUserId(event.target.value.toLowerCase());
+            }}
+            type="text"
+            className="search"
+            placeholder="User ID"
+            ></input>
+    
+            <button onClick={handleLogin} id="searchBtn">
+            Login
+            </button>
+        </div>
     )
 }
 
