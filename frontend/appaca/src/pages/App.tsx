d855cec85490e95b5dd62e5c5078d6db8c06b9e5
dpkingii <dupopking@gmail.com>
2025-04-20 12:57:04 -0400

    added a simple login component

--- frontend/appaca/src/pages/App.tsx
+++ frontend/appaca/src/pages/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './LandingPage'
 import LoginPage from './LoginPage'
 import NotFound from './NotFound'
+import DisplayPage from './DisplayPage'
 
 function App() {
 
@@ -13,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage/>}/>
       <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/display" element={<DisplayPage/>} />
       <Route path="/notFound" element={<NotFound/>}/>
     </Routes>
   )
