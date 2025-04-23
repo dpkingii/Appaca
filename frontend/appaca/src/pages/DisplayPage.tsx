0a954a36000c9ecf43bec49bde93a8492d544241
Aisling Greene <aislinggreene@MacBookAir.localdomain>
2025-04-22 20:41:26 -0400

    twotruthsbasics

--- frontend/appaca/src/pages/DisplayPage.tsx
+++ frontend/appaca/src/pages/DisplayPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 
 
 function DisplayPage() {
-    
+    const navigate = useNavigate();
+    const handleLogin = async()=> { navigate('/twoTruths')};
     return (
         <>
             <h1>DisplayPage</h1>
+                <button onClick = {handleLogin}
+                id = "searchBtn"> Login </button>
         </>
     )
 }
