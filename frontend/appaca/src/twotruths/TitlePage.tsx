3c5adb740c296257ddf1ec217fc25ba74873bfa5
Aisling Greene <aislinggreene@Aislings-MacBook-Air.local>
2025-04-23 19:41:40 -0400

    grass

--- frontend/appaca/src/twotruths/TitlePage.tsx
+++ frontend/appaca/src/twotruths/TitlePage.tsx
@@ -15,6 +15,22 @@ function TitlePage() {
     return (
         <div className="title-page-container"> 
             <div className="title-page-content"> 
+                 <img 
+                    src="https://png.pngtree.com/png-vector/20220713/ourmid/pngtree-insect-ladybug-cartoon-bugs-character-png-image_5932864.png" 
+                    alt="Cartoon bug graphic" 
+                    className="title-image"
+                  />
+
+                 <img 
+                    src="https://static.vecteezy.com/system/resources/thumbnails/018/133/052/small/natural-green-grass-bushes-decorate-environmental-ecology-cartoon-scene-png.png" 
+                    alt="Cartoon grass graphic" 
+                    className="grass-image"
+                  />
+                                   <img 
+                    src="https://static.vecteezy.com/system/resources/thumbnails/018/133/052/small/natural-green-grass-bushes-decorate-environmental-ecology-cartoon-scene-png.png" 
+                    alt="Cartoon grass graphic" 
+                    className="grass-image2"
+                  />
                 <h1>Two Truths and a Bug</h1>
                 <button onClick={handleStartClick}>Start</button>
             </div>
