fc0e60b310d268b31ccb01f62942c20a68b3d84c
dpkingii <dupopking@gmail.com>
2025-04-23 18:59:53 -0400

    update title page css

--- frontend/appaca/src/twotruths/TitlePage.tsx
+++ frontend/appaca/src/twotruths/TitlePage.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './TitlePage.css'; 
+import { useNavigate } from 'react-router-dom'
+
 
 function TitlePage() {
+
+    const navigate = useNavigate();
+
+    const handleStartClick = () => {
+        navigate('/game'); // Navigate to the /game path
+    };
+
+
     return (
-        <div className="title-page">
-            <h1>Two Truths and a Bug</h1>
+        <div className="title-page-container"> 
+            <div className="title-page-content"> 
+                <h1>Two Truths and a Bug</h1>
+                <button onClick={handleStartClick}>Start</button>
+            </div>
         </div>
     );
 }
