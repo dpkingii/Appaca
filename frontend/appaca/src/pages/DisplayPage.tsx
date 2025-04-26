4a79d144ed3f27006c824e3ba4358db1ba7d63d8
Selina Li <selinali168888@gmail.com>
2025-04-26 17:05:30 -0400

    Added Display page and its CSS

--- frontend/appaca/src/pages/DisplayPage.tsx
+++ frontend/appaca/src/pages/DisplayPage.tsx
@@ -1,12 +1,54 @@
 import React from 'react'
 import { useState } from "react";
-
+import "./DisplayPage.css";
 
 function DisplayPage() {
     
     return (
         <>
-            <h1>DisplayPage</h1>
+            <div className = "all">
+
+                <div className = "topBar">
+                    <h1>Appaca!</h1>
+                    <h2>@profilename</h2>
+                </div>
+
+                <div className = "content">
+                    <div className = "sideBar">
+                        <img src="" alt="Alpaca Streak Icon" />
+                        <h3>Streak: 5</h3>
+
+                        <h3>Mentor</h3>
+                        <p>insert mentor name</p>
+
+                        <h3>Group Members</h3>
+                        <p>- @name 1</p>
+                        <p>- @name 2</p>
+                        <p>- @yourname</p>
+
+                    </div>
+                    
+                    <div className = "leaderboard">
+                        <h2>Leader Board</h2>
+                        <div className = "backboard">
+
+                            <div className = "board">
+                                
+                                <div className = "entry"> <p>01) @name</p> <div className = "number">88</div> </div>
+                                <div className = "entry"> <p>02) @name</p> <div className = "number">79</div> </div>
+                                <div className = "entry"> <p>03) @name</p> <div className = "number">60</div> </div>
+                                <div className = "entry"> <p>04) @name</p> <div className = "number">43</div> </div>
+                                <div className = "entry"> <p>05) @name</p> <div className = "number">08</div> </div>
+        
+                            </div>
+
+                        </div>
+                        <p>Your Place: XX</p>
+                    </div>
+
+                </div>
+
+            </div>
         </>
     )
 }
