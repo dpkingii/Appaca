275ba0dc0a985c34b2203ad389b183bcd1cf93db
Vincent Liu <vincentliu0136@gmail.com>
2025-04-26 16:45:05 -0400

    Changed app for MongoDB

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from motor.motor_asyncio import AsyncIOMotorClient
 
-DATABASE_URL = "sqlite:///./test.db"
+MONGO_URI = "mongodb://localhost:27017"
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["appaca_db"]
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+def get_db():
+    return db
