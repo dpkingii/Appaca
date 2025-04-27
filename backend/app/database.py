24a439409ba390eb2f73f04e3561ed86f49584de
Vincent Liu <vincentliu0136@gmail.com>
2025-04-27 18:55:11 -0400

    Fixed Database Issues

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
 
-MONGO_URI = "mongodb://localhost:27017"
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["appaca_db"]
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+client = AsyncIOMotorClient(MONGODB_URI)
+db = client["Appaca"]
 
 def get_db():
     return db
