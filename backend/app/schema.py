0318e06b43e0001807aa096da2567e4cc1e2a59c
Vincent Liu <vincentliu0136@gmail.com>
2025-04-27 20:13:35 -0400

    Added Message Creation

--- backend/app/schema.py
+++ backend/app/schema.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class NewUser(BaseModel):
     username: str
     email: str
     password: str
     role: str
+
+class Message(BaseModel):
+    sender_username: str
+    recipient_username: str
+    content: str
+    timestamp: datetime = None
